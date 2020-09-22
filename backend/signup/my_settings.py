15796e045cad6ce6d866d54d46efbc57d368e95b
kuidoli <kuidoli@hanmail.net>
2020-09-22 14:56:22 +0900

    backend

---
+++ backend/signup/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'babygoat',
+        'USER': 'root',
+        'PASSWORD': 'ssafy',
+        'HOST': 'j3b304.p.ssafy.io',
+        'PORT': '3306',
+    }
+}
