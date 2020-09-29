8e7a93443c893f87499df3737c48da554ae5a1e9
정현희 <wjdgusgml997@naver.com>
2020-09-29 16:31:13 +0900

    S03P23B304-105 [+]UPDATE mainpage Carousel CSS

--- frontend/src/components/Carousel.vue
+++ frontend/src/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="content" style="width:100%;">
     <carousel  :per-page="1" :mouse-drag="false" :autoplay="true" :autoplayTimeout="5000" :loop="true" :autoplayHoverPause="true" :paginationPosition="bottom-overlay">
-      <!-- <slide v-for="(item, index) in storeData" :key="index" :index="index"> -->
-        <!-- <img :src="item.store_id" alt="item.store_id" /> -->
-      <slide>
-        <img style="height:25vh!important ; width: 250vh" src="../assets/image/background.jpg">
+      <slide v-for="(item, index) in storeData" :key="index" :index="index">
+        <img :src="item.store_id" alt="item.store_id" />
+      <!-- <slide> -->
+        <!-- <img style="height:25vh!important ; width: 250vh" src="../assets/image/background.jpg"> -->
       </slide>
     </carousel>
   </div>
