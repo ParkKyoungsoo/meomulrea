39d13b9a9f551ceda2e532c72f157d9a5c30ee54
kuidoli <kuidoli@hanmail.net>
2020-10-04 14:01:34 +0900

    pull from master and push to feature

---
+++ backend/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('user_username/', views.user_username, name='user_usernmae'),
+    path('user_email/', views.user_email, name='user_email'),
+    path('user_detail/', views.user_detail, name='user_detail'),
+    path('profile/<int:user_id>/', views.profile, name='profile'),
+    path('user_order_list/', views.user_order_list, name='user_order_list'),
+    path('user_order/', views.user_order, name='user_order'),
+]
---
+++ frontend/src/components/party.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-btn text to="/create">
+      <v-icon left>mdi-forum</v-icon>
+      <div class="hidden-xs-only">파티만들기</div>
+    </v-btn>
+    <v-btn text to="/discover">
+      <v-icon left>mdi-chat</v-icon>
+      <div class="hidden-xs-only">파티찾기</div>
+    </v-btn>
+    <div>
+      <ChatList />
+    </div>
+  </div>
+</template>
+<script>
+import ChatList from "./Chat/ChatList.vue";
+
+export default {
+  components: {
+    ChatList,
+  },
+};
+</script>
