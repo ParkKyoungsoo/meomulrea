19d319a7231fc6e2af4a9b5f7c9b8e22ec9a4be0
정현희 <wjdgusgml997@naver.com>
2020-10-05 12:50:59 +0900

    [-]DELETE backend cuase cache error

--- backend/chatroom/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import makeChatroom
-
-class ChatRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = makeChatroom
-        fields = '__all__'
--- backend/deliveries/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'deliveries'
-
-urlpatterns = [
-    path('input_data/', views.save_delivery)
-]
