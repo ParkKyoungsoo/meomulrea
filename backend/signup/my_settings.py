2f48bb4ad7174f508784777a367be894cf1e790f
정현희 <wjdgusgml997@naver.com>
2020-10-05 12:56:23 +0900

    [+]ADD backend from feature/algorithm

---
+++ backend/signup/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'babygoat',
+        'USER': 'root',
+        'PASSWORD': 'ssafy',
+        'HOST': 'ec2-54-180-109-206.ap-northeast-2.compute.amazonaws.com',
+        'PORT': '3306',
+    }
+}
